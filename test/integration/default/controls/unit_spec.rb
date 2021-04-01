3d8abeeda011ffcde92b946e5599ea5201395ead
Tobias Jungel <tobias.jungel@gmail.com>
2021-04-01 21:53:40 +0200

    feat(unit): add dropin capability for unit files
    This allows to create drop-ins for unit files as described in
    https://www.freedesktop.org/software/systemd/man/systemd.unit.html.
    
    Either create a full unit or create a dropin for an existing unit to
    alter some details.
    
    Caveat: no top-level drop-ins are supported

--- test/integration/default/controls/unit_spec.rb
+++ test/integration/default/controls/unit_spec.rb
@@ -22,6 +22,7 @@ control 'Systemd units' do
     its('group') { should eq 'root' }
     its('content') { should_not include '[status]' }
     its('content') { should_not include '[enabled]' }
+    its('content') { should_not include '[dropin]' }
     its('content') { should include 'Documentation=man:rsync(1) man:rsyncd.conf(5)' }
     its('content') { should include 'ConditionPathExists=/etc/rsyncd.conf' }
     its('content') { should include 'ConditionPathExists=/etc/passwd' }
@@ -33,5 +34,21 @@ control 'Systemd units' do
   describe service('rsync.service') do
     it { should be_enabled }
     it { should be_running }
+  end
+
+  describe file('/etc/systemd/system/systemd-journald.service.d/salt-override.conf') do
+    its('type') { should eq :file }
+    its('mode') { should cmp '0644' }
+    its('owner') { should eq 'root' }
+    its('group') { should eq 'root' }
+    its('content') { should_not include '[status]' }
+    its('content') { should_not include '[enabled]' }
+    its('content') { should_not include '[dropin]' }
+    its('content') { should include '[Unit]' }
+    its('content') { should include 'Description=Fancy new description' }
+    its('content') { should include '[Service]' }
+    its('content') { should include 'WatchdocSec=2min' }
+    its('content') { should include 'RestartSec=2' }
+    its('content') { should_not include '[Install]' }
   end
 end
